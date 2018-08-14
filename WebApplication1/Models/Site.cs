627ceeb4528cc734586b1000af0898221e41ed9b
themoodie <moodie.jacob96@gmail.com>
2018-08-14 13:54:39 -0400

    Added User and Site classes

---
+++ WebApplication1/Models/Site.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WebApplication1.Models
+{
+    public class Site
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string URL { get; set; }
+        public string Description { get; set; }
+        public virtual User User { get; set; }
+    }
+}
